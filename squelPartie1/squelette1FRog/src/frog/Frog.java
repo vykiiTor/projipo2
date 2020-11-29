5f6b1449cf3051bd465731f267189dab67822bcd
vykiiTor <vykiitor@gmail.com>
2020-11-29 17:30:25 +0100

    apres cours 1

--- squelPartie1/squelette1FRog/src/frog/Frog.java
+++ squelPartie1/squelette1FRog/src/frog/Frog.java
@@ -2,10 +2,34 @@ package frog;
 
 import gameCommons.Game;
 import gameCommons.IFrog;
+import util.Case;
+import util.Direction;
 
 public class Frog implements IFrog {
 	
 	private Game game;
+	private Direction dir;
+	private Case pos;
+
+	public Frog (Game game){
+		this.game = game;
+		this.pos = new Case(game.width/2,0);
+		this.dir = Direction.up;
+	}
 
 
+	@Override
+	public Case getPosition() {
+		return this.pos;
+	}
+
+	@Override
+	public Direction getDirection() {
+		return this.dir;
+	}
+
+	@Override
+	public void move(Direction key) {
+
+	}
 }
